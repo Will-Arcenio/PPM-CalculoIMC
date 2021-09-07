7f7188df8e0ff2472ed5a063cbba8adee729c50d
Willian Arcênio <willianarcenio@hotmail.com>
2021-09-07 19:17:56 -0300

    Ajuste no Script para mostrar o resultado do cálculo e a classificação.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ function calculaImc() {
     // Pega peso e altura
     let altura         = parseFloat(document.getElementById('altura').value);
     let peso           = parseFloat(document.getElementById('peso').value);
+    if (Number.isInteger(altura)) {
+        altura /= 100;
+    }
     let imc            = 0;
-    let eClassificacao = document.querySelector('#classificacao');
+    let getOldClassi   = document.querySelector('span#classificacao').className;
+    let eClassificacao = document.querySelector('span#classificacao');
     let classiName     = '';
     let classificacao;
 
@@ -21,60 +25,64 @@ function calculaImc() {
     }
 
     imc = peso/(altura * altura);
-
     // Classificação
-    switch (imc) {
-        case (imc < 16):
-            eClassificacao.classList.toggle('');
-            classificacao = 'magreza-grave';
-            classiName = 'Magreza Grave';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 16) && (imc < 17)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'magreza-moderada';
-            classiName = 'Magreza Moderada';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 17) && (imc < 18.5)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'magreza-leve';
-            classiName = 'Magreza Leve';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 18.5) && (imc < 25)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'saudavel';
-            classiName = 'Saudável';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 25) && (imc < 30)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'sobrepeso';
-            classiName = 'Sobrepeso';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 30) && (imc < 35)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'obesidade-grau-1';
-            classiName = 'Obesidade Grau I';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case ((imc >= 35) && (imc < 40)):
-            eClassificacao.classList.toggle('');
-            classificacao = 'obesidade-grau-2';
-            classiName = 'Obesidade Grau II';
-            eClassificacao.classList.toggle(classificacao);
-            break;
-        case (imc >= 40):
-            eClassificacao.classList.toggle('');
-            classificacao = 'obesidade-grau-3';
-            classiName = 'Obesidade Grau III';
-            eClassificacao.classList.toggle(classificacao);
-            break;
+    if (imc < 16) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'magreza-grave';
+        classiName = 'Magreza Grave';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 16 && imc < 17) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'magreza-moderada';
+        classiName = 'Magreza Moderada';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 17 && imc < 18.5) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'magreza-leve';
+        classiName = 'Magreza Leve';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 18.5 && imc < 25) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'saudavel';
+        classiName = 'Saudável';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 25 && imc < 30) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'sobrepeso';
+        classiName = 'Sobrepeso';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 30 && imc < 35) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'obesidade-grau-1';
+        classiName = 'Obesidade Grau I';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 35 && imc < 40) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'obesidade-grau-2';
+        classiName = 'Obesidade Grau II (severa)';
+        eClassificacao.classList.add(classificacao);
+    } else if (imc >= 40) {
+        if (getOldClassi != '') {
+            eClassificacao.classList.remove(getOldClassi);
+        }
+        classificacao = 'obesidade-grau-3';
+        classiName = 'Obesidade Grau III (mórbida)';
+        eClassificacao.classList.add(classificacao);
     }
-
-    console.log(classiName);
 
     // Add a informação nos campos
     document.getElementById('imc-value').innerHTML = imc.toFixed(2);
